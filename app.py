64bfbb9061f9ee8640d62c87cbfd72337468a883
Madineni-Karthika <131964560+Madineni-Karthika@users.noreply.github.com>
2024-02-14 19:14:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from PIL import Image
 import ast
 
-flag_data = pd.read_excel("C:/Users/karthika2001/OneDrive/Documents/overallanalysis.xlsx")
+flag_data = pd.read_excel("overallanalysis.xlsx")
 all_keywords = sorted(set(flag_data['Keyword'].tolist())) 
 st.set_page_config(page_title='Flag Data Viewer', layout='wide', initial_sidebar_state='expanded')
 logo = Image.open('Logo.jpg')  
